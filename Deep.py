b244236e05a60055cecfaa397faaadf453305812
11Leen <leenshorman824@gmail.com>
2026-02-11 19:23:46 +0300

    Add input check for the answer to life

---
+++ Deep.py
@@ -0,0 +1,7 @@
+answer = input("what is the answer to the great question of life , the universe and everything:")
+
+if answer == "42" or answer == "forty_two" or answer == "forty two":
+    print("yes")
+
+else:
+    print("No")
