1b4ad2c44fe0d52584bbd0667e359ab62078804e
leen <11Leen>
2026-02-11 19:44:32 +0300

    final upload for cs50 files

---
+++ mealtime.py
@@ -0,0 +1,23 @@
+def main():
+    # أضفت مسافة هنا بعد علامة الاستفهام لتجنب تداخل الكلام
+    answer = input("What time is it? ")
+    time = convert(answer)
+
+    # الشروط يجب أن تكون هكذا بدقة (بدون كلمة time == قبلها)
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
+
+def convert(time):
+    # فصل الساعات والدقائق
+    hours, minutes = time.split(":")
+    
+    return float(hours) + float(minutes) / 60
+
+if __name__ == "__main__":
+    main()
---
+++ savingbank.py
@@ -0,0 +1,10 @@
+greeting  = str(input("Enter you greeting:")).strip().lower()#عشان ما يكون فراغات ولا احرف كبيرة
+
+if greeting.startswith == "hello":
+    print("0$")
+
+elif greeting.startswith("h"):
+    print("20$")
+
+else:
+    print("100$")
